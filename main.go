5724a012122cefedd6211f1dea7b8726a0b5a048
SiglMarkus <43786665+SiglMarkus@users.noreply.github.com>
2019-02-14 12:19:09 +0100

    Update main.go
    added comments

--- main.go
+++ main.go
@@ -8,12 +8,17 @@ import (
 )
 
 func main() {
-	handle, err := pcap.OpenLive("enp0s3", 1600, true, pcap.BlockForever)
+	//Choose Network Interface
+	device := "enp0s3"
+	//Create Packet Sniffer
+	handle, err := pcap.OpenLive(device, 1600, true, pcap.BlockForever)
+
 	if err != nil {
 		panic(err)
 	}
 
 	//Filter only IP Protocol packages
+	//Created through tcpdump -dd "ip"
 	bpfInstructions := []pcap.BPFInstruction{
 		{ 0x28, 0, 0, 0x0000000c },
 		{ 0x15, 0, 1, 0x00000800 },
@@ -21,13 +26,31 @@ func main() {
 		{ 0x6, 0, 0, 0x00000000 },
 	}
 
+
+	//could also use handle.CompileBPFFilter("ip")
+	//but we first tried it with the binary from tcpdump
 	if err := handle.SetBPFInstructionFilter(bpfInstructions); err != nil {
 		panic(err)
 	}
 
+	//get all ip packages
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 
+	//print packet
 	for packet := range packetSource.Packets() {
+		/*
+		for better display, here should be a few lines of string manipulation or accessing the
+		relevant data direct through methods - if available
+
+		important information would be for example:
+		DestMAC
+		SrcMAC
+		DestIP
+		SrcIP
+		DestPort
+		SrcPort
+		packet.content
+		*/
 		fmt.Println(packet.String())
 	}
 }
